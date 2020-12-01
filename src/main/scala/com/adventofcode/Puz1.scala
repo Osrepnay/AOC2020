29dcca6a7af21a5105b9ae479dcd542a88eeee82
Osrepnay <lleerraaff@gmail.com>
2020-12-01 14:12:00 -0600

    Changed day 1 to use Set instead of List

--- src/main/scala/com/adventofcode/Puz1.scala
+++ src/main/scala/com/adventofcode/Puz1.scala
@@ -4,7 +4,7 @@ import scala.io.Source
 
 object Puz1{
 	def main(args: Array[String]): Unit = {
-		val lines=Source.fromFile("input.txt").mkString("").split( "\n").toList.map(_.toDouble)
+		val lines=Source.fromFile("input.txt").mkString("").split( "\n").map(_.toDouble).toSet
 		for(num <- lines) {
 			for(num2 <- lines
 				if lines contains 2020-num-num2){
