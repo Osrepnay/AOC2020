1eefe4fa40f3b52d29faac75d1bcb7faa44a1df5
Osrepnay <lleerraaff@gmail.com>
2020-12-02 17:10:12 -0600

    Removed unnecessary escape in regex for day 2

--- src/main/scala/com/adventofcode/Puz2.scala
+++ src/main/scala/com/adventofcode/Puz2.scala
@@ -5,7 +5,7 @@ import scala.io.Source
 object Puz2{
 	def main(args: Array[String]): Unit = {
 		val fileLines=Source.fromURL(getClass.getResource("/input2.txt")).mkString("").split("\n").toList
-		val fileLinesSplit=fileLines.map(line => line.split("\\-|: | ").toList)
+		val fileLinesSplit=fileLines.map(line => line.split("-|: | ").toList)
 		val numRight=fileLinesSplit.count(
 			line => line.last.charAt(line(0).toInt-1).toString==line(2) ^
 				line.last.charAt(line(1).toInt-1).toString==line(2)
