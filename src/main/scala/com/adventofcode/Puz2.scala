1394169b7f3029c6b3c6903789124d1c0937310c
Osrepnay <lleerraaff@gmail.com>
2020-12-02 17:09:18 -0600

    Changed day 2 to use .count instead of for loop

--- src/main/scala/com/adventofcode/Puz2.scala
+++ src/main/scala/com/adventofcode/Puz2.scala
@@ -5,14 +5,11 @@ import scala.io.Source
 object Puz2{
 	def main(args: Array[String]): Unit = {
 		val fileLines=Source.fromURL(getClass.getResource("/input2.txt")).mkString("").split("\n").toList
-		val fileLinesSplit=fileLines.map(line => line.split(" ").toList)
-		val rightPasswords=
-			for(line <- fileLinesSplit;
-				range=line(0).split("-").toList.map(_.toInt-1);
-				letter=line(1).head;
-				pass=line(2)
-				if pass.charAt(range(0))==letter ^ pass.charAt(range(1))==letter)
-				yield line
-		println(rightPasswords.length)
+		val fileLinesSplit=fileLines.map(line => line.split("\\-|: | ").toList)
+		val numRight=fileLinesSplit.count(
+			line => line.last.charAt(line(0).toInt-1).toString==line(2) ^
+				line.last.charAt(line(1).toInt-1).toString==line(2)
+		)
+		println(numRight)
 	}
 }
