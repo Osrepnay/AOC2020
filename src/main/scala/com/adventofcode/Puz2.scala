c74cf49056939014f9afc48f489d6afd2472c274
Osrepnay <lleerraaff@gmail.com>
2020-12-02 10:07:45 -0600

    Gave each day a separate file for inputs and moved inputs to resources folder

--- src/main/scala/com/adventofcode/Puz2.scala
+++ src/main/scala/com/adventofcode/Puz2.scala
@@ -4,13 +4,13 @@ import scala.io.Source
 
 object Puz2{
 	def main(args: Array[String]): Unit = {
-		val fileLines=Source.fromFile("input.txt").mkString("").split("\n").toList
+		val fileLines=Source.fromURL(getClass.getResource("/input2.txt")).mkString("").split("\n").toList
 		val fileLinesSplit=fileLines.map(line => line.split(" ").toList)
 		val rightPasswords=
 			for(line <- fileLinesSplit;
 				range=line(0).split("-").toList.map(_.toInt-1);
 				letter=line(1).head;
-				pass=line(2);
+				pass=line(2)
 				if pass.charAt(range(0))==letter ^ pass.charAt(range(1))==letter)
 				yield line
 		println(rightPasswords.length)
