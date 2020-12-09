d87cb671adcaf53fddb2b409b73a2b813e226bda
Osrepnay <lleerraaff@gmail.com>
2020-12-09 11:39:35 -0600

    Redid formatting

--- src/main/scala/com/adventofcode/Puz9.scala
+++ src/main/scala/com/adventofcode/Puz9.scala
@@ -3,15 +3,15 @@ package com.adventofcode
 import scala.io.Source
 import scala.util.{Failure, Success, Using}
 
-object Puz9{
+object Puz9 {
 
 	def main(args: Array[String]): Unit = {
-		val nums = Using(Source.fromURL(getClass.getResource("/input9.txt"))){
+		val nums = Using(Source.fromURL(getClass.getResource("/input9.txt"))) {
 			source => source.mkString("").split("\n").map(_.toLong).toList
 		}
 		nums match {
 			case Success(nums) =>
-				val idxs = nums.indices.map{
+				val idxs = nums.indices.map {
 					idxS =>
 						(idxS, nums.takeRight(nums.length - idxS - 1).indices.
 							filter(idxL => nums.slice(idxS, idxL + idxS + 1).sum == 542529149).map(_ + idxS + 1))
